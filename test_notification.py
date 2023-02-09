38a6a63c172390779b520bd94da706eea0df587c
Rezwanur Rahman <rezwan.cse09@gmail.com>
2023-02-09 16:56:23 -0700

    sorted trips

--- test_notification.py
+++ test_notification.py
@@ -9,5 +9,22 @@ def send_push():
     return "Tried to send!"
 
 
+def get_text(item):
+    text = "Hi..." + item['user_name'] + ",\nThese are some information that might help you about your trip\n:compass:"\
+           +item['src_address']\
+           +" to " + item['dst_address']\
+           +"\n" + ":alarm_clock: in 10 minutes\n"\
+           +"=============================\n:boom:Road Accidents:car:\n=============================\n"\
+           +"None reported\n"\
+           +"=============================\n:male-construction-worker:Construction Events :female-construction-worker:\n=============================\n"\
+           +"None reported\n"\
+           +"=============================\n:motorway:Road Conditions:motorway:\n=============================\n"\
+           +"None reported\n"\
+           +"=============================\n:sunny:Weather:cloud:\n=============================\n"\
+           +"None reported\n"
+    return text
+
+
 # Test the function
-send_push()
+print(get_text({"user_name":"name", "src_address":"src-addr", "dst_address":"dst_addr"}))
+#send_push()
